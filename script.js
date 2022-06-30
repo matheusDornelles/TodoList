bbee3f7f5d40e6221655414c24e6a485f5981daa
Matheus <mdmaia91@gmail.com>
2022-06-29 22:09:14 -0300

    todolist

---
+++ script.js
@@ -0,0 +1,29 @@
+const listElement = document.querySelector('ul')
+const inputElement = document.querySelector('input')
+const buttonElement = document.querySelector('button')
+
+const tasks = []
+
+function showTasks(){
+    
+    listElement.innerHTML = ''
+    
+    for (task of tasks){
+        const taskElement = document.createElement('ul')
+        const text = document.createTextNode(task)
+
+        taskElement.appendChild(text)
+        listElement.appendChild(taskElement)
+    }
+}
+showTasks();
+
+function addTask(){
+    const text = inputElement.value
+    tasks.push(text)
+    inputElement.value = ''
+
+    showTasks()
+}
+
+buttonElement.setAttribute('onclick','addTask()')
